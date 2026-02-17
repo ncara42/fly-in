a122630057fcf15b65aea0fd5de32208bd95b5fb
Noel Caravaca Gil <ncaravac@c1r15s3.42madrid.com>
2026-02-17 15:32:02 +0100

    In progress: line: 77

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 from typing import TypedDict, Any
 from enum import Enum
-from src.Colors import *
+from src.Colors import Color, Palette
+import heapq
+import math
+
 
 class Dron(TypedDict):
     id: str
@@ -16,6 +19,7 @@ class Hub(TypedDict):
     max_drones: int
     zone: str
 
+
 class Zones(Enum):
     NORMAL = "normal"
     BLOCKED = "blocked"
@@ -23,24 +27,63 @@ class Zones(Enum):
     PRIORITY = "priority"
 
 
-def find_path(adjacencies: dict[str, list[str]], start: str, end: str) -> list[Any]:
-    queue: list[list[str]] = [[start]]
-    visited: set[Any] = set()
+def heuristic(hubs: dict[str, Hub], node: str, end: str) -> float:
+    # Distancias (euclidiana) entre coordenadas de cada nodo
+    # Parte del algoritmo A*. Superior a BFS: Explora todos los caminos al mismo tiempo
+    # Superior A*: Calcula el coste de cada ruta
+    # A* Elegirá entre la suma más baja
+    (x1, y1) = hubs[node]['coord']
+    (x2, y2) = hubs[end]['coord']
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def find_path(hubs: dict[str, Hub], adjacencies: dict[str, list[str]], start: str, end: str) -> list[Any]:
+    
+    # Como Python siempre mira el primer elemento de una tupla
+    # para decidir cual es menor, uso 0 al principio
+    queue = [(0, start)]
+
+    # came_from va a guardar el camino que se va a tomar desde el final
+    # hasta el inicio. Luego se le dará la vuelta 
+    came_from = {}
+    g_score: dict[str, float] = {node: float('inf') for node in hubs}
+    g_score[start] = 0
 
     while queue:
-        path: list[str] = queue.pop(0)
-        node = path[-1]
+        # Uso _ porque no usaré esta variable, solo
+        # para desempaquetar el valor que me interesa en
+        # current, que es justo la que uso
+        # heapq.heappop saca el más pequeño de la lista
+        # ej. queue = [(8.07, 'B'), (4.23, 'C')]
+        # Resultado:
+        # _ valdrá 4.23
+        # current valdrá 'C'
+        _, current = heapq.heappop(queue)
 
-        if node == end:
-            return path
+        # Ahora suponiendo que current vale C y end vale D
+        # tengo que comprobar si C es el final. Cuando sea
+        # el final, este bloque se ejecutará
+        # Este bloque da la vuelta a came_from, por lo que
+        # obtengo el path de inicio a fin
+        if current == end:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            return path[::-1]
 
-        if node not in visited:
-            for neighbor in adjacencies.get(node, []):
-                if neighbor not in path:
-                    new_path = list(path)
-                    new_path.append(neighbor)
-                    queue.append(new_path)
-            visited.add(node)
+
+        ###### Aquí me quedo. Seguir comentando aquí
+        for neighbor in adjacencies.get(current, []):
+            tentative_g = g_score[current] + 1
+            
+            if tentative_g < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g
+                f_score = tentative_g + heuristic(hubs, neighbor, end)
+                heapq.heappush(queue, (f_score, neighbor))
+
     return []
 
 
@@ -81,8 +124,8 @@ def parse_map(map_path: str):
             if line.startswith('nb_drones'):
                 nb_drones: int = int(line.split(':')[1])  # type: ignore
             
-            # Identificar variable 'start_hub, end_hub': Coordenadas
-            elif line.startswith(('start_hub', 'end_hub')):
+            # Identificar variable 'start_hub, end_hub', 'hub': Coordenadas
+            elif line.startswith(('hub', 'start_hub', 'end_hub')):
                 parts: list[str] = line.split(':')
                 x_hub: str = parts[0]
                 data: list[str] = parts[1].split()
@@ -122,9 +165,7 @@ def parse_map(map_path: str):
                 # IA??? Tengo mis dudas sobre esto honestamente:
                 # Me lo hizo por un error de tipado con hubs (bloque de arriba)
                 # pero no tengo claro por qué tomó la decision de guardar esta info
-                # Store the parsed hub in the hubs mapping
                 hubs[path] = hub
-                print(hubs)
 
             # Crear una lista de nodos conectados:
             # Dónde pueden acceder (por eso usamos .append()!)
@@ -140,10 +181,10 @@ def parse_map(map_path: str):
                 adjacencies[v].append(u)
 
     # Aquí ocurre la solución
-    path_solved = find_path(adjacencies, dron['start_hub'], dron['end_hub'])
+    path_solved = find_path(hubs, adjacencies, dron['start_hub'], dron['end_hub'])
     dron['path'] = path_solved
-    print(adjacencies)
     print(path_solved)
+
 
 def main() -> None:
     if len(sys.argv) < 2:
